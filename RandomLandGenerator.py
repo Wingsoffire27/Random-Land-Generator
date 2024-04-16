d346c641348c2a28164da9b08feaec61cc4bbada
Wingsoffire27 <160536344+Wingsoffire27@users.noreply.github.com>
2024-04-16 16:21:59 -0500

    U#1 RandomLandGenerator.py
    added random tree Hight
    changed min of pic len and hight

--- RandomLandGenerator.py
+++ RandomLandGenerator.py
@@ -1,11 +1,11 @@
 import random
 from PIL import Image
 #must be at least 10
-lenX = int(input("give me a number that is more then 10: "))
+lenX = int(input("give me a number that is more then 15: "))
 lenY = lenX
-if lenX < 10:
-    lenX = 10
-    lenY = 10
+if lenX < 15:
+    lenX = 15
+    lenY = 15
 
 groundX = []
 darkskyX = []
@@ -14,13 +14,15 @@ img = Image.new("RGB", (lenX,lenY), (135, 206, 235))
 def genGroundList():
     global groundX, lenY, lenX
     hight = round(lenY*0.7)
+    hight += random.randint(-3,3)
     for i in range(lenX):
         hight += random.randint(-1,1)
         groundX.append(hight)
 
 def genDarkSkyList():
     global darkskyX, lenY, lenX
-    hight = round(lenY*0.2)
+
+    hight = round(lenY/7)
     for i in range(lenX):
         hight += random.randint(-1,1)
         darkskyX.append(hight)
@@ -48,32 +50,36 @@ def trees():
     global groundX,lenX,lenY
     xVal = random.randint(3,lenX-3)
     yVal = groundX[xVal]-1
+    treeHight = 3
     #generate trunk
-    img.putpixel((xVal,yVal),(59, 38, 11))
-    img.putpixel((xVal, yVal-1), (59, 38, 11))
-    img.putpixel((xVal, yVal-2), (59, 38, 11))
+    for i in range(random.randint(3,10)):
+      img.putpixel((xVal,yVal-i),(59, 38, 11))
+      treeHight -= 1
+    #img.putpixel((xVal, yVal-1), (59, 38, 11))
+    #img.putpixel((xVal, yVal-2), (59, 38, 11))
     #generate leaves L2
-    img.putpixel((xVal+1, yVal-2), (96, 209, 36))
-    img.putpixel((xVal+2, yVal-2), (96, 209, 36))
-    img.putpixel((xVal-1, yVal-2), (96, 209, 36))
-    img.putpixel((xVal-2, yVal-2), (96, 209, 36))
+    img.putpixel((xVal+1, yVal-(2-treeHight)), (96, 209, 36))
+    img.putpixel((xVal+2, yVal-(2-treeHight)), (96, 209, 36))
+    img.putpixel((xVal-1, yVal-(2-treeHight)), (96, 209, 36))
+    img.putpixel((xVal-2, yVal-(2-treeHight)), (96, 209, 36))
     #generate leaves L2
-    img.putpixel((xVal, yVal-3), (96, 209, 36))
-    img.putpixel((xVal+1, yVal-3), (96, 209, 36))
-    img.putpixel((xVal+2, yVal-3), (96, 209, 36))
-    img.putpixel((xVal-1, yVal-3), (96, 209, 36))
-    img.putpixel((xVal-2, yVal-3), (96, 209, 36))
+    img.putpixel((xVal, yVal-(3-treeHight)), (96, 209, 36))
+    img.putpixel((xVal+1, yVal-(3-treeHight)), (96, 209, 36))
+    img.putpixel((xVal+2, yVal-(3-treeHight)), (96, 209, 36))
+    img.putpixel((xVal-1, yVal-(3-treeHight)), (96, 209, 36))
+    img.putpixel((xVal-2, yVal-(3-treeHight)), (96, 209, 36))
     #generate leaves L3 and 4
-    img.putpixel((xVal, yVal - 4), (96, 209, 36))
-    img.putpixel((xVal+1, yVal - 4), (96, 209, 36))
-    img.putpixel((xVal-1, yVal - 4), (96, 209, 36))
-    img.putpixel((xVal, yVal - 5), (96, 209, 36))
+    img.putpixel((xVal, yVal - (4-treeHight)), (96, 209, 36))
+    img.putpixel((xVal+1, yVal - (4-treeHight)), (96, 209, 36))
+    img.putpixel((xVal-1, yVal - (4-treeHight)), (96, 209, 36))
+    img.putpixel((xVal, yVal - (5-treeHight)), (96, 209, 36))
 
 
 genGroundList()
 genDarkSkyList()
 genAddColorOutlines()
 genFillOutlines()
-for i in range(round(1),round(random.randint(1,round(lenX/8)))):
+for i in range(round(1),round(random.randint(1,round(lenX/5)))):
     trees()
 img.show()
+img.save("out.png")
